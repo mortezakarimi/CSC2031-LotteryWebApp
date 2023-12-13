290cf62c0483d3e3250bb7c27a0e4994b7ddd675
Morteza Karimi <mortezak1373@gmail.com>
2023-12-13 20:00:34 +0330

    8.2

--- static/rng.js
+++ static/rng.js
@@ -1,14 +1,21 @@
 // JavaScript function to generate 6 random unique values in order and populate form
+
 function luckyDip() {
 
     // create empty set
     let draw = new Set();
 
     // while set does not contain 6 values, create a random value between 1 and 60
+    let min;
+    let max;
+    let value;
     while (draw.size < 6) {
         min = 1;
         max = 60;
-        value = Math.ceil(Math.random() * (max - min) + min);
+        let randomBuffer = new Uint32Array(1);
+        window.crypto.getRandomValues(randomBuffer);
+        let csRandomNumber = randomBuffer[0] / (0xFFFFFFFF);
+        value = Math.floor(csRandomNumber * (max - min) + min);
 
         // sets cannot contain duplicates so value is only added if it does not exist in set
         draw.add(value)
